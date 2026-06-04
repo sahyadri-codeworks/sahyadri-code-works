90247903e7a7090bff889109b0b86cd3ed6204fc
sahyadri-codeworks <dev.sahyadricodeworks@gmail.com>
2026-06-04 18:15:49 +0530

    Updates

---
+++ _next/static/UWUBqYwBuE7wgFQe_HqkT/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sahyadri-code-works//_next/:path+",
+        "destination": "/sahyadri-code-works/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
